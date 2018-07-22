4d1d252b18c1cac584d008b58bbd39a37ecc4ccd
alaskaa <sibylle.sehl@googlemail.com>
2018-07-22 15:18:57 +0100

    fetch data from hackernews API and make search results dynamic

--- src/App.js
+++ src/App.js
@@ -2,24 +2,13 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-const list = [{
-  title: 'Pooper\'s Adventures',
-  url: 'https://www.google.com',
-  author: 'Marcus Hooper',
-  num_comments: 9,
-  points: 15,
-  objectID: 0,
-}, {
-  title: 'Bille\'s Adventures',
-  url: 'https://www.twtter.com',
-  author: 'Bille Brown',
-  num_comments: 500,
-  points: 1000,
-  objectID: 1,
-}
-];
+const DEFAULT_QUERY = 'redux';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
 
-const person = "Sibylle";
+const url = `${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${DEFAULT_QUERY}`;
+console.log(url);
 
 const isSearched = searchTerm => item => item.title.toLowerCase().includes(searchTerm.toLowerCase());
 
@@ -30,14 +19,27 @@ class App extends Component {
 
 
     this.state = {
-      list,
-      name: person,
-      searchTerm: '',
+      result: null,
+      searchTerm: DEFAULT_QUERY,
 
     };
 
+    this.setSearchTopStories = this.setSearchTopStories.bind(this);
     this.onDismiss = this.onDismiss.bind(this);
     this.onSearchChange = this.onSearchChange.bind(this);
+  }
+
+  setSearchTopStories(result) {
+    this.setState({result});
+  }
+
+  componentDidMount() {
+    const { searchTerm } = this.state;
+
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+    .then(response => response.json())
+    .then(result => this.setSearchTopStories(result))
+    .catch(error => error);
   }
 
   onDismiss(id) {
@@ -51,7 +53,9 @@ class App extends Component {
   }
 
   render() {
-    const { searchTerm, list} = this.state;
+    const { searchTerm, result } = this.state;
+
+    if (!result) { return null; }
     return (
       <div className="page">
         <div className="interactions">
@@ -63,7 +67,7 @@ class App extends Component {
           </Search>
         </div>
         <Table
-          list={list}
+          list={result.hits}
           pattern={searchTerm}
           onDismiss={this.onDismiss}
         />
